0448f19293805c0018a61df0d4bd072af2e38022
Su Ruiyang <142382202+Su-Ruiyang@users.noreply.github.com>
2024-04-13 14:36:25 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,99 @@
+import datetime
+import sqlite3
+from flask import Flask, request, render_template, Markup
+import replicate
+import os
+
+os.environ["REPLICATE_API_TOKEN"] = "r8_JvkrVrg4jZb8ILdgJK6lKeufFECobjF3ZeNbR"
+
+app = Flask(__name__)
+
+name_flag = 0
+name=""
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    return(render_template("index.html"))
+
+@app.route("/main",methods=["GET","POST"])
+def main():
+    global name_flag, name
+    if name_flag == 0:
+        conn = sqlite3.connect("log.db")
+        c = conn.cursor()
+        name = request.form.get("name")
+        name_flag = 1
+        timestamp = datetime.datetime.now()
+        c.execute("insert into employee(name,timestamp) values(?,?)", (name, timestamp))
+        conn.commit()
+        c.close()
+        conn.close()
+    return(render_template("main.html",name=name))
+
+@app.route("/query",methods=["GET","POST"])
+def query():
+    conn = sqlite3.connect("log.db")
+    c = conn.execute("select * from employee")
+    r=""
+    for row in c:
+        r=r+str(row)+"<br>"
+    print(r)
+    r=Markup(r)
+    c.close()
+    conn.close()
+    return(render_template("query.html",r=r))
+
+@app.route("/delete",methods=["GET","POST"])
+def delete():
+    conn = sqlite3.connect("log.db")
+    c = conn.execute("delete from employee")
+    conn.commit()
+    c.close()
+    conn.close()
+    return(render_template("delete.html"))
+
+@app.route("/food_exp",methods=["GET","POST"])
+def food_exp():
+    return(render_template("food_exp.html"))
+
+@app.route("/prediction",methods=["GET","POST"])
+def prediction():
+    income = float(request.form.get("income"))
+    return(render_template("prediction.html",r=(income * 0.485)+147))
+
+
+@app.route("/music",methods=["GET","POST"])
+def music():
+    return(render_template("music.html"))
+
+@app.route("/music_generator",methods=["GET","POST"])
+def music_generator():
+    q = request.form.get("q")
+    r = replicate.run(
+        "meta/musicgen:7be0f12c54a8d033a0fbd14418c9af98962da9a86f5ff7811f9b3423a1f0b7d7",
+        input={
+            "prompt": q,
+            "duration": 30
+        }
+    )
+    return(render_template("music_generator.html",r=r))
+
+@app.route("/ethical_test",methods=["GET","POST"])
+def ethical_test():
+    return(render_template("ethical_test.html"))
+
+@app.route("/answer",methods=["GET","POST"])
+def answer():
+    ans = request.form["options"] #Might need to change to Square bracket
+    print(ans)
+    if ans == 'TRUE':
+        return(render_template("wrong.html"))
+    else:
+        return(render_template("correct.html"))
+
+@app.route("/end",methods=["GET","POST"])
+def end():
+    return(render_template("end.html"))
+
+if __name__ == "__main__":
+    app.run()
